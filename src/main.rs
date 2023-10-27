cfb03ac2c0f3919b88cf31fc74fc23415b4ec99b
SimonCROS <scros@student.42lyon.fr>
2023-10-27 18:37:42 +0200

    Save

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use std::mem::size_of;
+
 use anyhow::{Context, Result};
 use ash::{
     self,
-    vk::{self, DeviceQueueCreateInfo},
+    vk::{
+        self, BufferCreateInfo, CommandBufferAllocateInfo, CommandPoolCreateFlags,
+        CommandPoolCreateInfo, DeviceQueueCreateInfo,
+    },
 };
 
 fn main() -> Result<()> {
@@ -44,11 +49,38 @@ fn main() -> Result<()> {
         let create_info = vk::DeviceCreateInfo::builder()
             .queue_create_infos(&queue_create_infos)
             .build();
-        unsafe { instance.create_device(physical_device, &create_info, None) }
-    }?;
+        unsafe { instance.create_device(physical_device, &create_info, None) }?
+    };
 
-    let queue = unsafe { device.get_device_queue(0, 0) };
+    // let queue = unsafe { device.get_device_queue(0, 0) };
 
+    let command_pool = {
+        let create_info = CommandPoolCreateInfo::builder()
+            .queue_family_index(queue_family_index)
+            .build();
+        unsafe { device.create_command_pool(&create_info, None) }?
+    };
+
+    let command_buffers = {
+        let allocate_info = CommandBufferAllocateInfo::builder()
+            .command_pool(command_pool)
+            .command_buffer_count(1)
+            .build();
+        unsafe { device.allocate_command_buffers(&allocate_info) }?
+    };
+
+    let buffer = {
+        let create_info = BufferCreateInfo::builder()
+            .size(16 * std::mem::size_of::<i32>() as vk::DeviceSize)
+            .usage(vk::BufferUsageFlags::UNIFORM_BUFFER)
+            .build();
+        unsafe { device.create_buffer(&create_info, None) }?
+    };
+
+    unsafe { device.destroy_buffer(buffer, None) }
+    unsafe { device.free_command_buffers(command_pool, &command_buffers) }
+    unsafe { device.destroy_command_pool(command_pool, None) }
+    unsafe { device.destroy_device(None) }
     unsafe { instance.destroy_instance(None) }
     Ok(())
 }
