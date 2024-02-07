3f5ce2a1ddfd900ded603af07b7da8c7073500b8
SimonCROS <scros@student.42lyon.fr>
2024-02-07 15:43:35 +0100

    chore: load swapchain

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -2,10 +2,13 @@ pub mod device;
 pub mod swapchain;
 pub mod window;
 
-use std::ffi::CString;
+use std::ffi::{self, c_char, CString};
 
 use anyhow::Result;
-use ash::vk;
+use ash::{
+    extensions::ext::{self},
+    vk,
+};
 
 use raw_window_handle::HasRawDisplayHandle;
 use window::RendererWindow;
@@ -15,14 +18,14 @@ use winit::{
     keyboard::{Key, NamedKey},
 };
 
-use self::device::RendererDevice;
+use self::{device::RendererDevice, swapchain::RendererSwapchain};
 
 pub struct Renderer {
     pub instance: ash::Instance,
     // pub debug: RendererDebug,
     pub window: RendererWindow,
     pub main_device: RendererDevice,
-    // pub swapchain: RendererSwapchain,
+    pub swapchain: RendererSwapchain,
     // pub render_pass: vk::RenderPass,
     // pub graphics_pipeline: RendererPipeline,
     // pub command_pools: CommandPools,
@@ -30,24 +33,44 @@ pub struct Renderer {
 }
 
 impl Renderer {
+    fn used_layer_names() -> Vec<ffi::CString> {
+        vec![ffi::CString::new("VK_LAYER_KHRONOS_validation").unwrap()]
+    }
+
+    fn used_extensions() -> Vec<*const c_char> {
+        vec![ext::DebugUtils::name().as_ptr()]
+    }
+
     pub fn new() -> Result<Self> {
         let (event_loop, window) = RendererWindow::create_window()?;
 
         let entry = unsafe { ash::Entry::load() }?;
 
-        let extension_name_pts =
-            ash_window::enumerate_required_extensions(window.raw_display_handle())?;
+        let layers_names = Self::used_layer_names();
+        let layers_names_raw: Vec<*const c_char> =
+            layers_names.iter().map(|l| l.as_ptr()).collect();
 
-        let instance = Self::create_instance(&entry, &vec![], extension_name_pts)?;
+        let mut extension_names = Self::used_extensions();
+        extension_names.extend_from_slice(ash_window::enumerate_required_extensions(
+            window.raw_display_handle(),
+        )?);
+
+        let instance = Self::create_instance(&entry, &layers_names_raw, &extension_names)?;
 
         let window = RendererWindow::new(event_loop, window, &entry, &instance)?;
 
         let main_device = RendererDevice::new(&instance)?;
 
+        let render_pass = Self::create_render_pass(&main_device, &window)?;
+
+        let mut swapchain = RendererSwapchain::new(&instance, &main_device, &window)?;
+        swapchain.create_framebuffers(&main_device, render_pass)?;
+
         Ok(Self {
             instance,
             window,
             main_device,
+            swapchain,
         })
     }
 
@@ -78,8 +101,8 @@ impl Renderer {
 
     fn create_instance(
         entry: &ash::Entry,
-        layer_name_pts: &[*const i8],
-        extension_name_pts: &[*const i8],
+        used_layer_names: &Vec<*const c_char>,
+        used_extensions: &Vec<*const c_char>,
     ) -> Result<ash::Instance> {
         let app_name = CString::new("Vulkan App")?;
         let engine_name = CString::new("Vulkan Engine")?;
@@ -93,16 +116,68 @@ impl Renderer {
 
         let instance_info = vk::InstanceCreateInfo::builder()
             .application_info(&app_info)
-            .enabled_extension_names(extension_name_pts)
-            .enabled_layer_names(layer_name_pts);
+            .enabled_extension_names(used_extensions)
+            .enabled_layer_names(used_layer_names);
 
         let instance = unsafe { entry.create_instance(&instance_info, None)? };
 
         Ok(instance)
     }
 
+    fn create_render_pass(
+        device: &RendererDevice,
+        window: &RendererWindow,
+    ) -> Result<vk::RenderPass> {
+        let surface_formats = window.formats(device.physical_device)?;
+        let surface_format = surface_formats.first().unwrap();
+
+        let attachments = [vk::AttachmentDescription::builder()
+            .format(surface_format.format)
+            .samples(vk::SampleCountFlags::TYPE_1)
+            .load_op(vk::AttachmentLoadOp::CLEAR)
+            .store_op(vk::AttachmentStoreOp::STORE)
+            .stencil_load_op(vk::AttachmentLoadOp::DONT_CARE)
+            .stencil_store_op(vk::AttachmentStoreOp::DONT_CARE)
+            .initial_layout(vk::ImageLayout::UNDEFINED)
+            .final_layout(vk::ImageLayout::PRESENT_SRC_KHR)
+            .build()];
+
+        let color_attachment_references = [vk::AttachmentReference::builder()
+            .attachment(0)
+            .layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL)
+            .build()];
+
+        let subpasses = [vk::SubpassDescription::builder()
+            .pipeline_bind_point(vk::PipelineBindPoint::GRAPHICS)
+            .color_attachments(&color_attachment_references)
+            .build()];
+
+        let subpass_dependencies = [vk::SubpassDependency::builder()
+            .src_subpass(vk::SUBPASS_EXTERNAL)
+            .src_stage_mask(vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT)
+            .dst_subpass(0)
+            .dst_stage_mask(vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT)
+            .dst_access_mask(
+                vk::AccessFlags::COLOR_ATTACHMENT_READ | vk::AccessFlags::COLOR_ATTACHMENT_WRITE,
+            )
+            .build()];
+
+        let render_pass_info = vk::RenderPassCreateInfo::builder()
+            .attachments(&attachments)
+            .subpasses(&subpasses)
+            .dependencies(&subpass_dependencies);
+
+        let render_pass = unsafe {
+            device
+                .logical_device
+                .create_render_pass(&render_pass_info, None)
+        }?;
+
+        Ok(render_pass)
+    }
+
     /// Renders a frame for our Vulkan app.
-    unsafe fn render(&mut self, window: &RendererWindow) -> Result<()> {
+    unsafe fn render(&mut self, _window: &RendererWindow) -> Result<()> {
         unimplemented!()
     }
 
--- src/renderer/swapchain.rs
+++ src/renderer/swapchain.rs
@@ -1,11 +1,13 @@
+use core::slice;
+
 use anyhow::Result;
 use ash::{
     self,
     extensions::khr::{self, Swapchain},
     vk::{
-        CompositeAlphaFlagsKHR, Format, ImageAspectFlags, ImageSubresourceRange, ImageUsageFlags,
-        ImageView, ImageViewCreateInfo, ImageViewType, PresentModeKHR, SharingMode,
-        SwapchainCreateInfoKHR, SwapchainKHR,
+        self, CompositeAlphaFlagsKHR, Format, ImageAspectFlags, ImageSubresourceRange,
+        ImageUsageFlags, ImageView, ImageViewCreateInfo, ImageViewType, PresentModeKHR,
+        SharingMode, SwapchainCreateInfoKHR, SwapchainKHR,
     },
     Instance,
 };
@@ -16,13 +18,15 @@ pub struct RendererSwapchain {
     pub swapchain: SwapchainKHR,
     pub swapchain_loader: khr::Swapchain,
     pub image_views: Vec<ImageView>,
+    pub framebuffers: Vec<vk::Framebuffer>,
+    pub extent: vk::Extent2D,
 }
 
 impl RendererSwapchain {
     pub fn new(
         instance: &Instance,
-        device: RendererDevice,
-        window: RendererWindow,
+        device: &RendererDevice,
+        window: &RendererWindow,
     ) -> Result<Self> {
         let graphics_queue_family = device.main_graphics_queue_family();
 
@@ -39,7 +43,7 @@ impl RendererSwapchain {
             let swapchain_info = SwapchainCreateInfoKHR::builder()
                 .surface(window.surface)
                 .min_image_count(
-                    3.clamp(capabilities.min_image_count, capabilities.max_image_count),
+                    3.max(capabilities.min_image_count).min(capabilities.max_image_count),
                 )
                 .image_format(surface_format.format)
                 .image_color_space(surface_format.color_space)
@@ -89,7 +93,36 @@ impl RendererSwapchain {
             swapchain,
             swapchain_loader,
             image_views,
+            framebuffers: vec![],
+            extent: capabilities.current_extent,
         })
+    }
+
+    pub fn create_framebuffers(
+        &mut self,
+        device: &RendererDevice,
+        render_pass: vk::RenderPass,
+    ) -> Result<()> {
+        self.framebuffers.reserve(self.image_views.len());
+
+        for image_view in &self.image_views {
+            let framebuffer_info = vk::FramebufferCreateInfo::builder()
+                .render_pass(render_pass)
+                .attachments(slice::from_ref(image_view))
+                .width(self.extent.width)
+                .height(self.extent.height)
+                .layers(1);
+
+            let framebuffer = unsafe {
+                device
+                    .logical_device
+                    .create_framebuffer(&framebuffer_info, None)
+            }?;
+
+            self.framebuffers.push(framebuffer);
+        }
+
+        Ok(())
     }
 
     pub unsafe fn cleanup(&self, device: RendererDevice) {
@@ -97,6 +130,7 @@ impl RendererSwapchain {
             device.logical_device.destroy_image_view(*image_view, None);
         }
 
-        self.swapchain_loader.destroy_swapchain(self.swapchain, None);
+        self.swapchain_loader
+            .destroy_swapchain(self.swapchain, None);
     }
 }
