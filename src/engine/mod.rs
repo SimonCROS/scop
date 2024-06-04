bb341eca2897c25c943dc3378d002e75f755ee5c
Simon Cros <simon.dc.cros@gmail.com>
2024-06-05 00:15:27 +0200

    Remove old manually generated pyramid

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -50,79 +50,10 @@ impl Engine {
     }
 
     pub fn run(&mut self) -> Result<()> {
-        // let vertices = vec![
-        //     Vertex {
-        //         position: [-0.448, -0.448, -0.448].into(),
-        //         color: [0.0, 0.0, 1.0].into(),
-        //         normal: Vector3::up(),
-        //         uv: Vector2::zero(),
-        //     },
-        //     Vertex {
-        //         position: [0.448, -0.448, -0.448].into(),
-        //         color: [0.25, 0.0, 0.0].into(),
-        //         normal: Vector3::up(),
-        //         uv: Vector2::zero(),
-        //     },
-        //     Vertex {
-        //         position: [0.448, -0.448, 0.448].into(),
-        //         color: [0.5, 0.0, 0.0].into(),
-        //         normal: Vector3::up(),
-        //         uv: Vector2::zero(),
-        //     },
-        //     Vertex {
-        //         position: [-0.448, -0.448, 0.448].into(),
-        //         color: [0.75, 0.0, 0.0].into(),
-        //         normal: Vector3::up(),
-        //         uv: Vector2::zero(),
-        //     },
-        //     Vertex {
-        //         position: [0.0, 0.448, 0.0].into(),
-        //         color: [0.5, 1.0, 1.0].into(),
-        //         normal: Vector3::up(),
-        //         uv: Vector2::zero(),
-        //     },
-        // ];
-
-        // let indices = vec![
-        //     0, 1, 2, 0, 3, 2, // Bottom
-        //     0, 1, 4,
-        //     1, 2, 4,
-        //     2, 3, 4,
-        //     3, 0, 4];
-
-        // let m = Rc::new(
-        //     Mesh::builder(self.renderer.main_device.clone())
-        //         .vertices(&vertices)
-        //         .indices(&indices)
-        //         .build()?,
-        // );
-
         let m = Rc::new(read_obj_file(
             self.renderer.main_device.clone(),
             "./resources/teapot2.obj",
         )?);
-
-        // let xs: [f32; 40] = [
-        //     0.02, 0.59, 0.88, 0.43, 0.01, 0.21, 0.10, 0.98, 0.62, 0.15, 0.12, 0.42, 0.61, 0.41,
-        //     0.22, 0.58, 0.99, 0.33, 0.79, 0.90, 0.39, 0.48, 0.31, 0.28, 0.80, 0.51, 0.30, 0.07,
-        //     0.53, 0.35, 0.32, 0.27, 0.60, 0.73, 0.49, 0.91, 0.25, 0.45, 0.40, 0.92,
-        // ];
-        // let ys: [f32; 40] = [
-        //     0.50, 0.44, 0.95, 0.68, 0.55, 0.00, 0.82, 0.13, 0.85, 0.40, 0.60, 0.25, 0.09, 0.89,
-        //     0.05, 0.27, 0.22, 0.20, 0.04, 0.74, 0.14, 0.65, 0.03, 0.48, 0.07, 0.45, 0.62, 0.23,
-        //     0.99, 0.24, 0.17, 0.00, 0.08, 0.54, 0.80, 0.98, 0.59, 0.83, 0.96, 0.61,
-        // ];
-        // for i in 0..40 {
-        //     GameObject::builder(self)
-        //         .name("Hello World")
-        //         .mesh(m.clone())
-        //         .transform(Transform {
-        //             translation: [xs[i] * 8. - 4., ys[i] * 8. - 4., ys[(i + 20) % 40] * 8.].into(),
-        //             scale: Vector3::one(),
-        //             rotation: Vector3::zero(),
-        //         })
-        //         .build();
-        // }
 
         GameObject::builder(self)
             .name("Hello World")
